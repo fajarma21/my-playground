b5788c5ecd457a9ad80e9958e720027b4388b940
Fajar Maulana <fajarma21@gmail.com>
2025-01-17 15:52:21 +0700

    feat(init): init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <div className="root-container">{children}</div>
       </body>
     </html>
   );
