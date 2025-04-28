6f7dff7959a4fb24d7c861495e223094b2d5fe16
Fajar Maulana <fajarma21@gmail.com>
2025-04-28 18:13:04 +0700

    test2

--- src/app/pokemon/detail/[name]/page.tsx
+++ src/app/pokemon/detail/[name]/page.tsx
@@ -1,1 +1,33 @@
-export { default } from "./View";
+import MainImage from "./components/MainImage";
+import { getPokemonData, getPokemons } from "./View.helpers";
+import { PokemonDetailProps, PokemonListResult } from "./View.types";
+import { PokemonData } from "@/app/pokemon/types";
+import Content from "./components/Content";
+
+const PokemonDetail = async ({ params }: PokemonDetailProps) => {
+  const { name } = await params;
+  const data: PokemonData = await getPokemonData(name);
+  const { id = 0, moves = [], stats = [], types = [] } = data || {};
+
+  return (
+    <>
+      {Boolean(id) && <MainImage id={id} name={name} />}
+      <Content
+        id={id}
+        isError={false}
+        isLoading={false}
+        moves={moves}
+        name={name}
+        stats={stats}
+        types={types}
+      />
+    </>
+  );
+};
+
+export async function generateStaticParams() {
+  const list: PokemonListResult = await getPokemons();
+  return list.results.map(({ name }) => ({ name }));
+}
+
+export default PokemonDetail;
