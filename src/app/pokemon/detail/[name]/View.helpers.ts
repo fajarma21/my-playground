448242a573a994dea4ee209b8956dac8fb050469
Fajar Maulana <fajarma21@gmail.com>
2025-04-28 17:16:11 +0700

    feat(pokemon): add generateStaticParams

--- src/app/pokemon/detail/[name]/View.helpers.ts
+++ src/app/pokemon/detail/[name]/View.helpers.ts
@@ -1,5 +1,15 @@
 import { POKEMON_API } from "@/app/pokemon/constants/url";
 
+export async function getPokemons() {
+  try {
+    const response = await fetch("${POKEMON_API}/pokemon");
+    if (response.ok) return await response.json();
+    throw new Error("Not Found");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 export const getPokemonData = async (value: string) => {
   try {
     const response = await fetch(`${POKEMON_API}/pokemon/${value}`);
