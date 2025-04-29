ad5cd8e2d1915c9afdd3d024fee92fc7423bfa58
Fajar Maulana <fajarma21@gmail.com>
2025-04-29 14:04:07 +0700

    feat(pokemon): change back config

--- src/app/pokemon/detail/[name]/View.helpers.ts
+++ src/app/pokemon/detail/[name]/View.helpers.ts
@@ -1,15 +1,5 @@
 import { POKEMON_API } from "@/app/pokemon/constants/url";
 
-export async function getPokemons() {
-  try {
-    const response = await fetch(`${POKEMON_API}/pokemon`);
-    if (response.ok) return await response.json();
-    throw new Error("Not Found");
-  } catch (error) {
-    console.log(error);
-  }
-}
-
 export const getPokemonData = async (value: string) => {
   try {
     const response = await fetch(`${POKEMON_API}/pokemon/${value}`);
--- src/app/pokemon/detail/[name]/View.types.ts
+++ src/app/pokemon/detail/[name]/View.types.ts
@@ -1,11 +1,3 @@
 export interface PokemonDetailProps {
   params: Promise<{ name: string }>;
 }
-
-interface PokemonList {
-  name: string;
-}
-
-export type PokemonListResult = {
-  results: PokemonList[];
-};
